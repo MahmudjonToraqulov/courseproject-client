378e1977f8be1d1c0cfc80d6f3b34620a39c969d
Mahmudjon <toraqulovmahmudjon@gmail.com>
2024-10-07 09:23:05 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2> Hello World!!! </h2>
+      <h2 onClick={() => setCount(count + 1)}> Count {count} </h2>
+    </>
+  )
+}
+
+export default App
